6d3d81581ab1d39024470119cf896130244a5f17
juaho <juaho@student.hive.fi>
2025-09-16 18:55:01 +0300

    True vibecode regex monstrosity. TODO: rewrite all

--- lua/nvim-function-linecount.lua
+++ lua/nvim-function-linecount.lua
@@ -17,15 +17,17 @@ function M.count_function_lines()
 	local namespace = vim.api.nvim_create_namespace("error_namespace")
 	local hl_group
 	local i = 1
+	local func_lines
 
 	vim.api.nvim_buf_clear_namespace(buffer, namespace, 0, -1)
 	while i <= last_line do
 		local line = lines[i]
 
 		if string.match(line, "^.-%b()") then
+			func_lines = 0
 			local brace_line = i
 			local brace_on_same_line = string.match(lines[brace_line], "{")
-			while brace_line <= last_line and not brace_on_same_line do
+			while brace_line < last_line and not brace_on_same_line do
 				brace_line = brace_line + 1
 				brace_on_same_line = string.match(lines[brace_line], "{")
 			end
@@ -33,7 +35,11 @@ function M.count_function_lines()
 				break
 			end
 
-			local func_lines = 0
+			-- Catch code on the same line as the opening brace
+			if string.match(line, "{%s*%S+") and not string.find(line, "}") then
+				func_lines = func_lines + 1
+			end
+
 			local open_brackets = 0
 			for j = brace_line, last_line do
 				local current_line = lines[j]
@@ -48,7 +54,16 @@ function M.count_function_lines()
 				end
 			end
 
-			if not string.match(lines[brace_line], "{.*}") then
+			-- Catch code on the same line as the closing brace
+			if string.match(lines[i], "%s*%S+%s*};?%s*$") and not string.match(lines[i], "^.-%b()") then
+				func_lines = func_lines + 1
+			end
+
+			if string.match(lines[i], "^.-%b()") and string.match(lines[i], "{%s*%S.-}") then
+				func_lines = func_lines + 1
+			end
+
+			if not string.match(lines[brace_line], "%b{}") then
 				func_lines = func_lines - 1
 			end
 
@@ -61,7 +76,7 @@ function M.count_function_lines()
 			end
 
 			vim.api.nvim_buf_set_extmark(buffer, namespace, i - 1, 0, {
-				virt_text = { { string.format("-- Lines: %d", func_lines), hl_group } },
+				virt_text = { { string.format("-- Lines: %d", func_lines - 1), hl_group } },
 				virt_text_pos = "eol",
 			})
 		end
