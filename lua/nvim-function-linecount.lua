95b5f553a91991b39930a638513b5210d5a1e7d4
juaho <juaho@student.hive.fi>
2025-09-16 13:17:23 +0300

    gitried to fix C++ linecount

--- lua/nvim-function-linecount.lua
+++ lua/nvim-function-linecount.lua
@@ -19,40 +19,53 @@ function M.count_function_lines()
 	local i = 1
 
 	vim.api.nvim_buf_clear_namespace(buffer, namespace, 0, -1)
-    while (i < last_line) do
-        if string.match(lines[i], "^[%w%s_%*]+%s+[%w_%*]+%s*%b()") then
-			if not (string.match(lines[i], "{%s*$")) then
-				i = i + 1
+	while i <= last_line do
+		local line = lines[i]
+
+		if string.match(line, "^.-%b()") then
+			local brace_line = i
+			local brace_on_same_line = string.match(lines[brace_line], "{")
+			while brace_line <= last_line and not brace_on_same_line do
+				brace_line = brace_line + 1
+				brace_on_same_line = string.match(lines[brace_line], "{")
 			end
-			if (string.match(lines[i], "{%s*$")) then
-				local func_lines = 0
-				local open_brackets = 1
-				while (open_brackets > 0) do
-					i = i + 1
-					func_lines = func_lines + 1
-					if (i > last_line) then
-						return
-					end
-					if (string.match(lines[i], "{%s*$")) then
-						open_brackets = open_brackets + 1
-					elseif (string.match(lines[i], "}%s*$")) then
-						open_brackets = open_brackets - 1
-					end
-				end
-				func_lines = func_lines - 1;
-				if (func_lines < M.line_limit) then
-					hl_group = "Nontext"
-				elseif (func_lines == M.line_limit) then
-					hl_group = "WarningMsg"
-				else
-					hl_group = "ErrorMsg"
-				end
-				vim.api.nvim_buf_set_extmark(buffer, namespace, i - 1, 0, {
-					virt_text = { { string.format("-- Lines: %d", func_lines), hl_group } },
-					virt_text_pos = "eol",
-				})
+			if brace_line > last_line then
+				break
 			end
-        end
+
+			local func_lines = 0
+			local open_brackets = 0
+			for j = brace_line, last_line do
+				local current_line = lines[j]
+				local opens, closes = select(2, current_line:gsub("{", "{")), select(2, current_line:gsub("}", "}"))
+				open_brackets = open_brackets + opens - closes
+
+				func_lines = func_lines + 1
+
+				if open_brackets == 0 then
+					i = j
+					break
+				end
+			end
+
+			if not string.match(lines[brace_line], "{.*}") then
+				func_lines = func_lines - 1
+			end
+
+			if func_lines < M.line_limit then
+				hl_group = "Nontext"
+			elseif func_lines == M.line_limit then
+				hl_group = "WarningMsg"
+			else
+				hl_group = "ErrorMsg"
+			end
+
+			vim.api.nvim_buf_set_extmark(buffer, namespace, i - 1, 0, {
+				virt_text = { { string.format("-- Lines: %d", func_lines), hl_group } },
+				virt_text_pos = "eol",
+			})
+		end
+
 		i = i + 1
 	end
 end
